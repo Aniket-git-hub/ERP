8364591079970f3d5793773f2fddc244d01cd3ee
aniket-git-hub <singhdharmvir81@gmail.com>
2024-01-05 10:57:43 +0530

    update

--- controllers/jobs/getJobsController.js
+++ controllers/jobs/getJobsController.js
@@ -1,15 +1,16 @@
 import getFilteredJobsService from "../../services/jobs/getFilteredJobsService.js";
 import generatePDF from "../../utils/generatePDF.js";
+import generateReportTemplate from "../../utils/pdfTemplates/reportTemplate.js";
 async function getJobsController(req, res, next) {
     const { page = 1, limit = 10, pdf } = req.query;
     const filters = req.query;
     try {
         const jobs = await getFilteredJobsService(parseInt(page), parseInt(limit), filters);
         if (pdf) {
-            console.log(pdf)
-            const pdfBuffer = await generatePDF(`<html><body>${JSON.stringify(jobs)}</body></html>`)
+
+            const pdfBuffer = await generatePDF(generateReportTemplate(jobs, req.query))
             res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename=report.pdf');
+            res.setHeader('Content-Disposition', `attachment; filename=${req.query.queryString}.pdf`);
             res.send(pdfBuffer);
         } else {
             res.json(jobs);
