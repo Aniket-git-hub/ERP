92f725481a7028f3a2df6ef5a7ef90daf6d3f678
aniket-git-hub <singhdharmvir81@gmail.com>
2024-01-09 17:21:56 +0530

    added csv generator

---
+++ utils/sendEmail.js
@@ -0,0 +1,19 @@
+import transporter from "../config/nodemailer.js";
+import getEnvVariable from "./env.js";
+
+async function sendEmail(email, subject, template) {
+    const mailOptions = {
+        from: getEnvVariable('NODEMAILER_USER'),
+        to: email,
+        subject: subject,
+        html: template
+    }
+    try {
+        const response = await transporter.sendMail(mailOptions)
+        return { response }
+    } catch (error) {
+        throw error
+    }
+}
+
+export default sendEmail;
